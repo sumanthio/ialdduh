39460f189df57ebad0a455f283a4c160552afe03
Sumanth <sumanth@bytemark.co>
2020-02-17 01:41:21 +0530

    Undo automation

--- src/containers/postContainer/index.js
+++ src/containers/postContainer/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "./node_modules/react";
-import { useParams } from "./node_modules/react-router-dom";
-import { Grid, Card } from "./node_modules/@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Grid, Card } from "@material-ui/core";
 
 import { getPostContent } from "../../services/placeholderApi";
 import { Comments } from "../../components/CommentComponent";
